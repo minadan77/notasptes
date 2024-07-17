6fc57d06b9729cddf2cd300b2dbe5622b690fd29
minadan77 <96655942+minadan77@users.noreply.github.com>
2024-07-17 09:14:57 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tareas-pendientes-v2';
+const CACHE_NAME = 'tareas-pendientes-v3';
 const urlsToCache = [
     '/',
     '/index.html',
@@ -26,19 +26,7 @@ self.addEventListener('fetch', (event) => {
                 if (response) {
                     return response;
                 }
-                return fetch(event.request).then(
-                    (response) => {
-                        if(!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-                        const responseToCache = response.clone();
-                        caches.open(CACHE_NAME)
-                            .then((cache) => {
-                                cache.put(event.request, responseToCache);
-                            });
-                        return response;
-                    }
-                );
+                return fetch(event.request);
             })
     );
 });
@@ -58,7 +46,6 @@ self.addEventListener('activate', (event) => {
     );
 });
 
-// Agregamos un evento message para manejar la actualización de la caché
 self.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'SKIP_WAITING') {
         self.skipWaiting();
