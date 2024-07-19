444ddf4b335dd063a2a845e6f2f7e6ca18f2aace
minadan77 <96655942+minadan77@users.noreply.github.com>
2024-07-19 11:18:47 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,7 @@ const urlsToCache = [
     '/index.html',
     '/styles.css',
     '/app.js',
+    '/manifest.json',
     '/icon-192x192.png',
     '/icon-512x512.png'
 ];
@@ -18,6 +19,38 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then((response) => response || fetch(event.request))
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(
+                    (response) => {
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(event.request, responseToCache);
+                            });
+                        return response;
+                    }
+                );
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
